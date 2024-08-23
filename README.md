4e042e08dbf489658c1955c009bf4724692f171b
Anilkumar Reddy <anilkreddy.a@gmail.com>
2024-08-23 02:58:07 -0400

    Initial setup of the action with typescript (for test)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# commerce-build
-Trigger the build in CCv2
+# SAP Commerce Cloud - Build
+
+This GitHub Action provides the following functionalities to perform build operations on SAP Commerce Cloud in the Public Cloud (CCv2) projects.
+
+- Trigger the build for the given tag or branch.
+- Send notifications to the given webhook URL or email address(es) on the build status.
+- Retry build if failed.
+
+## Getting Started
+
+See [action.yml](action.yml)
+
---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import * as core from '@actions/core'
+import axios from 'axios'
+
+export async function run(): Promise<void> {
+    try {
+        core.info('Triggering the CCv2 Cloud build');
+        const token: string = core.getInput('token');
+        const subscriptionCode: string = core.getInput('subscriptionCode');
+        const branch: string = core.getInput('branch');
+        const buildName: string = core.getInput('buildName');
+        const checkStatusInterval: string = core.getInput('checkStatusInterval');
+        const retryOnFailure: string = core.getInput('retryOnFailure');
+        const maxRetries: string = core.getInput('maxRetries');
+        const enableNotifications: string = core.getInput('enableNotifications');
+        const webhookUrl: string = core.getInput('webhookUrl');
+
+        core.info(`token: ${token}`);
+        core.info(`subscriptionCode: ${subscriptionCode}`);
+        core.info(`branch: ${branch}`);
+        core.info(`buildName: ${buildName}`);
+        core.info(`checkStatusInterval: ${checkStatusInterval}`);
+        core.info(`retryOnFailure: ${retryOnFailure}`);
+        core.info(`maxRetries: ${maxRetries}`);
+        core.info(`enableNotifications: ${enableNotifications}`);
+        core.info(`webhookUrl: ${webhookUrl}`);
+
+
+
+        const apiUrl = 'https://reqres.in/api/users'
+        try {
+            const response = await axios.get(apiUrl)
+            core.info(`Response status: ${response.status}`)
+            core.info(`Response data: ${JSON.stringify(response.data, null, 2)}`)
+        } catch (error) {
+            core.setFailed(`Error fetching data: ${(error as Error).message}`)
+        }
+
+        core.setOutput('time', new Date().toTimeString())
+    } catch (error) {
+        // Fail the workflow run if an error occurs
+        if (error instanceof Error) core.setFailed(error.message)
+    }
+}
