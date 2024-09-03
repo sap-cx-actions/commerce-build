b3308a647477a97a436896dcd6b4703224b8ce06
Anilkumar Reddy <anilkreddy.a@gmail.com>
2024-09-02 23:40:33 -0400

    Using ActionInput model and using util to name the build based on branch/tag if no build name is provided

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
-import { BuildRequest, BuildResponse, BuildStatus } from '@sap-cx-actions/models';
+import { BuildRequest, BuildResponse, BuildStatus, ActionInput } from '@sap-cx-actions/models';
 import { BuildService } from '@sap-cx-actions/commerce-services';
+import { getBuildName } from './utils';
 
 export async function run(): Promise<void> {
   let buildCode: string | undefined;
@@ -8,23 +9,25 @@ export async function run(): Promise<void> {
 
   try {
     core.info('Triggering the CCv2 Cloud build');
-    const token: string = core.getInput('token');
-    const subscriptionCode: string = core.getInput('subscriptionCode');
-    const branch: string = core.getInput('branch');
-    const buildName: string = core.getInput('buildName');
-    const checkStatusInterval: number = parseInt(core.getInput('checkStatusInterval'), 10);
-    const retryOnFailure: boolean = core.getInput('retryOnFailure') === 'true';
-    const maxRetries: number = parseInt(core.getInput('maxRetries'), 10);
-    const notify: boolean = core.getInput('notify') === 'true';
-    const webhookUrl: string = core.getInput('webhookUrl');
+    const input: ActionInput = {
+      token: core.getInput('token'),
+      subscriptionCode: core.getInput('subscriptionCode'),
+      branch: core.getInput('branch'),
+      buildName: await getBuildName(core.getInput('branch'), core.getInput('buildName')),
+      checkStatusInterval: parseInt(core.getInput('checkStatusInterval'), 10),
+      retryOnFailure: core.getBooleanInput('retryOnFailure'),
+      maxRetries: parseInt(core.getInput('maxRetries'), 10),
+      notify: core.getBooleanInput('notify'),
+      webhookUrl: core.getInput('webhookUrl')
+    };
 
-    const buildService = new BuildService(token, subscriptionCode);
+    const buildService = new BuildService(input.token, input.subscriptionCode);
 
     // Create a new build
     const buildRequest: BuildRequest = {
       applicationCode: 'commerce-cloud',
-      branch: branch,
-      name: buildName
+      branch: input.branch,
+      name: input.buildName
     };
 
     try {
@@ -36,12 +39,13 @@ export async function run(): Promise<void> {
       if (buildCode) {
         const getBuild: BuildResponse = await buildService.getBuild(buildCode);
         core.debug(`Get Build Response: ${JSON.stringify(getBuild, null, 2)}`);
-        buildStatus = getBuild.status; //TODO check while building if the status is BUILDING or something else
+        buildStatus = getBuild.status;
       }
 
-      if (notify && webhookUrl) {
+      if (input.notify && input.webhookUrl) {
         // TODO: Call notification service to send notification about build start.
         // Send notification about build start.
+        // notifier.sendNotification(Notification.BUILD_STARTED, getBuild);
       }
 
       core.setOutput('buildCode', buildCode);
